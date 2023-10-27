75021ce27dc309116000008dc43b791cec5a095d
Harshwardhan <harshwardhanshetty6@gmail.com>
2023-10-27 14:14:08 +0530

    Working on bike details page still

--- src/components/BikeDetails.js
+++ src/components/BikeDetails.js
@@ -12,12 +12,37 @@ const BikeDetails = () => {
               alt=""
             />
           </div>
-          <div className={`${styles.price}`}>PRice</div>
+          <div className={`${styles.price_varient}`}>
+            <div>Varient</div>
+            <div>Price</div>
+          </div>
         </div>
         <div className={`${styles.specs_features}`}>
           <h1 className={`${styles.header}`}>
             Raider 125 Specification and features
           </h1>
+          <div className={`${styles.specs}`}>Specifications</div>
+          <div className={`${styles.specs_table}`}>
+            <table border={1}>
+              <tbody>
+                <tr>
+                  <th>Power and Performance</th>
+                </tr>
+                <tr>
+                  <td>Displacement</td>
+                  <td>124.8 cc</td>
+                </tr>
+                <tr>
+                  <td>Max Power</td>
+                  <td>11.2 bhp @ 7500 rpm</td>
+                </tr>
+                <tr>
+                  <td>Max Torque</td>
+                  <td>11.2 Nm @ 6000 rpm</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
