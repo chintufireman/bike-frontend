f39e26a69cc73e1ed7530816658437c2dee225b1
YoruzuyaGintoki <88782377+YoruzuyaGintoki@users.noreply.github.com>
2023-10-30 16:26:29 +0530

    FInal card design decided

--- src/components/BikeCard.js
+++ src/components/BikeCard.js
@@ -27,6 +27,90 @@ const BikeCard = () => {
           <button> More Details </button>
         </div>
       </div>
+      <div className={`${styles.card}`}>
+        <Image src="/duke_390.jpg" alt="me" width="350" height="200" />
+        <div className={`${styles.title}`}>
+          <b>Duke 390 2024 new gen</b>
+        </div>
+        <div className={`${styles.price}`}>
+          <b>₹ 97,054</b> <span>Onwards</span>
+        </div>
+        <div className={`${styles.button}`}>
+          <button> More Details </button>
+        </div>
+      </div>
+      <div className={`${styles.card}`}>
+        <Image src="/duke_390.jpg" alt="me" width="350" height="200" />
+        <div className={`${styles.title}`}>
+          <b>Duke 390 2024 new gen</b>
+        </div>
+        <div className={`${styles.price}`}>
+          <b>₹ 97,054</b> <span>Onwards</span>
+        </div>
+        <div className={`${styles.button}`}>
+          <button> More Details </button>
+        </div>
+      </div>
+      <div className={`${styles.card}`}>
+        <Image src="/duke_390.jpg" alt="me" width="350" height="200" />
+        <div className={`${styles.title}`}>
+          <b>Duke 390 2024 new gen</b>
+        </div>
+        <div className={`${styles.price}`}>
+          <b>₹ 97,054</b> <span>Onwards</span>
+        </div>
+        <div className={`${styles.button}`}>
+          <button> More Details </button>
+        </div>
+      </div>
+      <div className={`${styles.card}`}>
+        <Image src="/duke_390.jpg" alt="me" width="350" height="200" />
+        <div className={`${styles.title}`}>
+          <b>Duke 390 2024 new gen</b>
+        </div>
+        <div className={`${styles.price}`}>
+          <b>₹ 97,054</b> <span>Onwards</span>
+        </div>
+        <div className={`${styles.button}`}>
+          <button> More Details </button>
+        </div>
+      </div>
+      <div className={`${styles.card}`}>
+        <Image src="/duke_390.jpg" alt="me" width="350" height="200" />
+        <div className={`${styles.title}`}>
+          <b>Duke 390 2024 new gen</b>
+        </div>
+        <div className={`${styles.price}`}>
+          <b>₹ 97,054</b> <span>Onwards</span>
+        </div>
+        <div className={`${styles.button}`}>
+          <button> More Details </button>
+        </div>
+      </div>
+      <div className={`${styles.card}`}>
+        <Image src="/duke_390.jpg" alt="me" width="350" height="200" />
+        <div className={`${styles.title}`}>
+          <b>Duke 390 2024 new gen</b>
+        </div>
+        <div className={`${styles.price}`}>
+          <b>₹ 97,054</b> <span>Onwards</span>
+        </div>
+        <div className={`${styles.button}`}>
+          <button> More Details </button>
+        </div>
+      </div>
+      <div className={`${styles.card}`}>
+        <Image src="/duke_390.jpg" alt="me" width="350" height="200" />
+        <div className={`${styles.title}`}>
+          <b>Duke 390 2024 new gen</b>
+        </div>
+        <div className={`${styles.price}`}>
+          <b>₹ 97,054</b> <span>Onwards</span>
+        </div>
+        <div className={`${styles.button}`}>
+          <button> More Details </button>
+        </div>
+      </div>
     </div>
   );
 };
